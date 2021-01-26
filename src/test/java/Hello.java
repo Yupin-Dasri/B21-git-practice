a2bf3077d9928669164bb762d4f1d2ebcc0f30af
Yupin-Dasri <74158072+Yupin-Dasri@users.noreply.github.com>
2021-01-26 15:27:13 -0500

    movie1 branch final work is done, ready to merge

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning git");
-        System.out.println("Ready to go back to living room");
+       //watching movie in bedroom
+        //ready to go back to living room
 
     }
 }
